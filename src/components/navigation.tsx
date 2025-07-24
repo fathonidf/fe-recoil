45880957f0ca455bfa9674fac4186b73dc599445
fathonidf <daffafathoni@gmail.com>
2025-07-25 01:07:02 +0700

    fix(navigation): update user role display to show 'agent' or 'member' based on user status

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -141,7 +141,7 @@ export function Navigation() {
                   <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
                   </svg>
-                  <span>member</span>
+                  <span>{user?.is_agent ? 'agent' : 'member'}</span>
                 </div>
               </div>
             </div>
@@ -218,7 +218,7 @@ export function Navigation() {
                 <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
                 </svg>
-                <span>member</span>
+                <span>{user?.is_agent ? 'agent' : 'member'}</span>
               </div>
             </div>
 
@@ -289,7 +289,7 @@ export function Navigation() {
                     <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                       <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
                     </svg>
-                    <span>member</span>
+                    <span>{user?.is_agent ? 'agent' : 'member'}</span>
                   </div>
                 </div>
                 <Link
