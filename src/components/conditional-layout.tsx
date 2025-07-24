643870868accccab5f0614d223bca20b1619a1a3
fathonidf <daffafathoni@gmail.com>
2025-07-24 15:19:33 +0700

    feat: add ConditionalLayout component with dynamic navigation and footer

---
+++ src/components/conditional-layout.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import type React from "react"
+import { usePathname } from "next/navigation"
+import { Navigation } from "@/components/navigation"
+import { Footer } from "@/components/footer"
+import BackgroundVectors from "@/components/background-vectors"
+
+export default function ConditionalLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname()
+  const showNavigationFooter = pathname !== '/login' && pathname !== '/register'
+
+  return (
+    <div className="min-h-screen flex flex-col relative overflow-x-hidden">
+      {/* Background Vectors with Parallax Effect */}
+      {showNavigationFooter && <BackgroundVectors />}
+
+      {/* Main Content */}
+      <div className="relative z-10 flex flex-col min-h-screen">
+        {showNavigationFooter && <Navigation />}
+        <main className="flex-1">
+          {children}
+        </main>
+        {showNavigationFooter && <Footer />}
+      </div>
+    </div>
+  )
+}
